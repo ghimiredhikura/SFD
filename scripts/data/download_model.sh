8449d0525c3fe2d8692ad56c8719ed6108a91818
ezetl <ezetl91@gmail.com>
2018-03-08 20:39:01 -0300

    fix wrong path in extraction of zip

--- scripts/data/download_model.sh
+++ scripts/data/download_model.sh
@@ -2,7 +2,7 @@
 root_dir=$(git rev-parse --show-toplevel)
 source $root_dir/scripts/data/datasets_utils.sh 
 
-output_dir="${root_dir}/../models/sfd_models/VGGNet/WIDER_FACE/"
+output_dir="${root_dir}/../"
 drive=$root_dir/scripts/data/google-drive.py
 
 mkdir -p $output_dir
